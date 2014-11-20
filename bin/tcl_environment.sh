e3ea424f7af035fb8a07aa70a52b614d18cfd4fc
ramgarg <raman.g.garg@oracle.com>
2014-11-20 20:01:53 +0530

    test 5

--- bin/tcl_environment.sh
+++ bin/tcl_environment.sh
@@ -2,6 +2,5 @@
 export TCL_VER="8.5.16.0.298506"
 export TCL_URL="https://s3-ap-southeast-1.amazonaws.com/heroku-buildtool-tcl/ActiveTcl8.5.16.0.298506-linux-x86_64-threaded.tar.gz"
 
-export VENDORING_DIR="/app/.buildpack"
-export TCL_HOME=$VENDORING_DIR/tcl-$TCL_VER
+export TCL_HOME="/app/vendor/tcl"
 export PATH=$PATH:$TCL_HOME/install/bin
